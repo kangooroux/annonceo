e2dc2949501689754e103c4a7b0fc7a5048ec944
Julien Robic <nayte91@gmail.com>
2020-08-06 22:41:51 +0200

    Structure suggestion for router and controllers

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,5 +6,23 @@ namespace App\Controller;
 
 class SecurityController
 {
+    public function __construct()
+    {
 
+    }
+
+    private function dispatch()
+    {
+
+    }
+
+    private function login()
+    {
+
+    }
+
+    private function register()
+    {
+
+    }
 }
